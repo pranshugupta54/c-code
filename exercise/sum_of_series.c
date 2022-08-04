28e6c420fd0c05ce9c4487b5e6b81aa446350fc3
Pranshu <pranshgupta54@gmail.com>
2022-08-04 22:37:37 +0530

    Sum of Series 1 - x^2/2! + x^4/4! ....

---
+++ exercise/sum_of_series.c
@@ -0,0 +1,50 @@
+/*
+18. Write a program in C to find the sum of the series [ 1-X^2/2!+X^4/4!- .........].
+
+*/
+# include<stdio.h>
+# include<math.h>
+
+int factorial(int i);
+
+int main(){
+  int n, product = 1; 
+  float x;
+  float sum = 1;
+  printf("Enter the value of x :");
+  scanf("%f",&x);
+  printf("Enter the number of terms :");
+  scanf("%d",&n);
+  
+  for (int i = 1; i <= n-1; i++)
+  {
+    float pow_of_x = (float)pow(x, 2 * i);
+    float sum_value = pow_of_x/factorial(2*i);
+    sum = sum + (pow(-1,i)) * sum_value;//(float)(pow (x,2 * i) / factorial(2*i));  
+    printf("sum%d:%f \n",i,sum);
+  
+  }
+  printf("Sum is %f", sum);
+  
+}
+
+int factorial(int m){
+  int prod = 1;
+  for (int i = 1; i <= m; i++)
+  {
+   prod = prod * i; 
+  }
+  return prod;
+}
+
+
+
+/* 
+
+x = 2
+n = 3
+
+1 - 2 + 2^4/24
+
+
+*/
