65d5f1679c54996749dc324c7a07bebb488ae54e
Pranshu <pranshgupta54@gmail.com>
2022-08-04 15:59:19 +0530

    .

--- exercise/code1.c
+++ exercise/code1.c
@@ -6,9 +6,13 @@ int main(){
   printf("Enter number:");
   scanf("%d",&n);
   
-  for (int i = 0; i <= n; i++)
+  for (int i = 1; i <= n; i++)
   {
-    
+    for (int j = 1; i <= i; j++)
+    {
+      printf("*");
+    }
+    printf("\n");
   }
   
 }
