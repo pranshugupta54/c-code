e7e7dd44749220f8620d39fd4a6478437798c84f
Pranshu <pranshgupta54@gmail.com>
2022-08-04 15:46:16 +0530

    dice

---
+++ dice.c
@@ -0,0 +1,58 @@
+# include<stdio.h>
+# include<math.h>
+
+int main(){
+
+  switch (expression)
+  {
+
+  
+  
+  case 1:
+
+  printf("+-----+ \n");
+	printf("|     | \n");
+  printf("|  0  | \n");
+	printf("|     | \n");
+	printf("+-----+ \n");
+  
+  case 2:
+
+  printf("+-----+ \n");
+	printf("|     | \n");
+  printf("| 0 0 | \n");
+	printf("|     | \n");
+	printf("+-----+ \n");
+
+
+  printf("+-----+ \n");
+	printf("|     | \n");
+  printf("|0 0 0| \n");
+	printf("|     | \n");
+	printf("+-----+ \n");
+
+  printf("+-----+ \n");
+	printf("| 0 0 | \n");
+  printf("|     | \n");
+	printf("| 0 0 | \n");
+	printf("+-----+ \n");
+
+  printf("+-----+ \n");
+	printf("| 0 0 | \n");
+  printf("|  0  | \n");
+	printf("| 0 0 | \n");
+	printf("+-----+ \n");
+  }
+
+
+
+}
+
+
+
+/*
+
+gcc armn.c
+./a.out
+
+*/
