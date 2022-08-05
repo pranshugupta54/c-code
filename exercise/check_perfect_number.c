5388eb82679fa7cf3ed8fc7a0cc9829d3c1a4715
Pranshu <pranshgupta54@gmail.com>
2022-08-05 10:34:24 +0530

    Perfect Number

---
+++ exercise/check_perfect_number.c
@@ -0,0 +1,45 @@
+/*
+
+27) check whether a given number is a perfect number or not.
+If sum of divisors == number then perfect number.
+*/
+
+# include<stdio.h>
+# include<math.h>
+
+int sq_check(int n);
+int main(){
+  int n, sum =0;
+  printf("Enter number:");
+  scanf("%d",&n);
+  
+  if (sq_check(n) == 1)
+  {
+    printf("%d is a perfect Square.\n",n);
+  }
+  else if (sq_check(n) == 0)
+  {
+    printf("%d is NOT a perfect square. \n",n);
+  }
+  else printf("Error");
+  
+  
+}
+
+int sq_check(int n){
+
+  for (int i = 0; i <= n; i++)
+  {
+    int sq_num = i * i;
+    if (sq_num == n)
+    {
+      return 1;
+      break;
+    }
+  }
+  return 0;
+
+}
+
+
+
