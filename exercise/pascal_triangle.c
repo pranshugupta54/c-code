b20c3641a7e4407671c98a96a44a97b5b4e5cf49
Pranshu <pranshgupta54@gmail.com>
2022-08-05 18:45:50 +0530

    Incomplete

--- exercise/pascal_triangle.c
+++ exercise/pascal_triangle.c
@@ -13,26 +13,14 @@ Example: Rows = 5;
 
 int main(){
   int row;
-  char a[] = " * "; //asterisk
   char sp[] = "   "; //space
   printf("Enter number of rows :");
   scanf("%d",&row);
-  
-  for (int i = 1; i <= row; i++)
-  {
-    for (int k = 1; k <= row-i; k++)
-    {
-      printf("%s", sp);
-    }
-    
-    for (int j = 1; j <= 2*i-1; j++)
-    {
-      printf("%s",a);
-    }
-    printf("\n");
-    
-  }
-
    
 }
 
+/*
+
+NOT COMPLETE
+
+*/
