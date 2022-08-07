cb2fb7ae1bbe1cbd1e856d004c607ab0fb90a913
Pranshu <pranshgupta54@gmail.com>
2022-08-07 16:02:10 +0530

    Update factorial.c

--- exercise/functions/factorial.c
+++ exercise/functions/factorial.c
@@ -6,12 +6,12 @@ Factorial Function
 # include<math.h>
 
 int factorial(int n);
-int main(){
-  int num;
-  printf("Enter a number: ");
-  scanf("%d",&num);
-  printf("Factorial of %d is %d.\n",num,factorial(num));
-}
+// int main(){
+//   int num;
+//   printf("Enter a number: ");
+//   scanf("%d",&num);
+//   printf("Factorial of %d is %d.\n",num,factorial(num));
+// }
 
 int factorial(int m){
   int prod = 1;
