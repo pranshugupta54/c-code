f92a4984adf632d893b8059e85a59938a56ab217
Pranshu <pranshgupta54@gmail.com>
2022-08-07 14:12:51 +0530

    Create sum_natural.c

---
+++ sum_natural.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+# include<math.h>
+
+int main(){
+  int n, sum =0;
+  printf("Enter number:");
+  scanf("%d",&n);
+  printf("The odd numbers are :");
+  for (int i = 1; i <= n; i++)
+  {
+    printf("%d ",2*i-1);
+    sum += 2*i-1;
+  }
+  printf("\n The sum of off natural number upto %d terms : %d \n",n,sum);
+
+}
+
+//test
+
+
+//test2
+
+
+//test 3
