b4e979e92b2f8989a1ad63cae214da774cbf8536
Pranshu <pranshgupta54@gmail.com>
2022-08-07 14:13:10 +0530

    Update code.c

--- code.c
+++ code.c
@@ -1,17 +1,16 @@
 # include<stdio.h>
 # include<math.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
-  int n, sum =0;
-  printf("Enter number:");
-  scanf("%d",&n);
-  printf("The odd numbers are :");
-  for (int i = 1; i <= n; i++)
-  {
-    printf("%d ",2*i-1);
-    sum += 2*i-1;
-  }
-  printf("\n The sum of off natural number upto %d terms : %d \n",n,sum);
+  srand(time(NULL));
+  printf("%d\n",rand()%6);
+  printf("%d\n",rand()%6);
+  printf("%d\n",rand()%6);
+  printf("%d\n",rand()%6);
+  printf("%d\n",rand()%6);
+  
 
 }
 
